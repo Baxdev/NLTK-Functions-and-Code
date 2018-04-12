af867d4ffb8ba7a7daf3b400f54521c85e3f845d
AldworthJohn <jbax.dev@gmail.com>
2018-04-12 19:00:58 +0800

    Adding comments to the functions

--- nltkFunctions.py
+++ nltkFunctions.py
@@ -1,5 +1,6 @@
 #Returns the length of the "set" of the text (i.e. how many different words/punctuation symbols it uses)
 #The set is all of the tokens in a text, with duplicate tokens removed.
+#Or you could say a set is all the types in a text
 #"Tokens" are a sequence of characters that are treated as a group.
 #Tokens can be things like words or punctuation symbols.
 def lexical_diversity(text):
@@ -7,6 +8,7 @@ def lexical_diversity(text):
 
 #Shows the percentage of unique types in a text.
 #A "type" is a unique item of vocabulary
+#ex: percentage(text1.count('car'), len(text1))
 def percentage(count, total):
     return 100 * count / total
 
